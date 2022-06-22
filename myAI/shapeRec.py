515c00ac6058c0125193f808b5da0eb1a5eafb08
sagi21805 <105317735+sagi21805@users.noreply.github.com>
2022-06-23 02:54:47 +0300

    Add files via upload

--- myAI/shapeRec.py
+++ myAI/shapeRec.py
@@ -1,9 +1,14 @@
 import torch
 import torch.nn as nn
+from torchvision import transforms
+import torch.optim as optim
+from PIL import Image
+import os
 
-class ShapeRecgognizer(nn.Module):
+PATH = "C:\\vscodeprojects\\python\\machine-learning\\myAI\\shape_data\\"
+class ShapeRecognizer(nn.Module):
     def __init__(self):
-        super(ShapeRecgognizer, self).__init__()
+        super(ShapeRecognizer, self).__init__()
         self.neuralNetwork = nn.Sequential(
             nn.Linear(784, 16),
             nn.ReLU(),
@@ -13,17 +18,49 @@ class ShapeRecgognizer(nn.Module):
             nn.ReLU(),
             nn.Linear(16, 16),
             nn.ReLU(),
-            nn.Linear(16, 10),
-            nn.ReLU(),
-            nn.Linear(10, 1)
+            nn.Linear(16, 1),
         )
     
     def forward(self, input):
         return self.neuralNetwork(input)
-        
-x = torch.rand(28, 28)
-x = x.reshape(784)
-print(x)
-REC = ShapeRecgognizer()
-output = REC(x)
-print(output)
+    
+   
+    def getData():
+        global I
+        for I in range(len(os.listdir(PATH))):
+                print(os.listdir(PATH)[I])
+                img = Image.open(PATH + os.listdir(PATH)[I])
+                convert_tensor = transforms.ToTensor()
+                x = convert_tensor(img)
+                yield x
+
+    def train(self):
+        self.loss = nn.MSELoss()
+        optimizer = optim.SGD(self.neuralNetwork.parameters(), lr=0.001, momentum=0.9)
+        for epoch in range(2):  # loop over the dataset multiple times
+
+            running_loss = 0.0
+            for i in range(len(os.listdir(PATH))):
+                # get the inputs; data is a list of [inputs, labels
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward + backward + optimize
+                outputs = self.forward(DATALIST[0])
+                loss = self.loss(outputs, torch.tensor([int(os.listdir(PATH)[I].split(".")[0])]))
+                loss.backward()
+                optimizer.step()
+
+                # print statistics
+                running_loss += loss.item()
+                if i % 2000 == 1999:    # print every 2000 mini-batches
+                    print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                    running_loss = 0.0
+
+
+REC = ShapeRecognizer()
+for image in ShapeRecognizer.getData():
+    DATALIST = [image.view(784), os.listdir(PATH)[I].split(".")[0]]
+REC.train()
+print("1")
---
+++ myAI/test.py
@@ -0,0 +1,33 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+input_size = 784 # 28x28
+hidden_size = 500 
+num_classes = 10
+num_epochs = 1
+batch_size = 64
+learning_rate = 0.001
+
+# MNIST dataset 
+train_dataset = torchvision.datasets.MNIST(root='./data', 
+                                           train=True, 
+                                           transform=transforms.ToTensor(),  
+                                           download=True)
+
+test_dataset = torchvision.datasets.MNIST(root='./data', 
+                                          train=False, 
+                                          transform=transforms.ToTensor())
+
+# Data loader
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                           batch_size=batch_size, 
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
+                                          batch_size=batch_size, 
+                                          shuffle=False)
+
+print(train_loader)
